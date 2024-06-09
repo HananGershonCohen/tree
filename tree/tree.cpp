38fcf133c3ceb5ac698a9b0dd9e2879d2070d47e
Hanan Gershon Cohen <hanang.cohen@gmail.com>
2024-06-09 22:14:22 +0300

    Add project files.

---
+++ tree/tree.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+struct Tnode {
+	int _val;
+	Tnode* _lson, * _rson;
+};
+
+int find_mid(Tnode* root)
+{
+	if (!root)
+		return 0;
+
+
+}
+
+int main()
+{
+	Tnode* root = nullptr;
+	find_mid(root);
+
+	return 0;
+}
