4077467719d1a474a14e7b487d9b4f9130310424
Hanan Gershon Cohen <hanang.cohen@gmail.com>
2024-06-19 14:36:22 +0300

    I add a new STRUCT to put the user's input in it, and also, I add the function print_InOrder to print. The program now works properly.

--- tree/ex13b.cc
+++ tree/ex13b.cc
@@ -1,5 +1,15 @@
 #include <iostream>
 using namespace std;
+//******
+//STRUCT
+//******
+
+struct Input
+{
+    int in_id;
+    int in_x;
+    int in_y;
+};
 
 struct Point {
     int x;
@@ -13,10 +23,51 @@ struct Node {
     Node* _right;
 };
 
+//===================================================================
+void insertRec(Node*& root, Input user) {
+    if (!root) {
+        root = new Node();
+        root->_id = user.in_id;
+        root->_data = new Point(); // הקצאת זיכרון עבור ה-Point
+        root->_data->x = user.in_x;
+        root->_data->y = user.in_y;
+        root->_left = root->_right = nullptr;
+        return;
+    }
 
+    if (user.in_id <= root->_id) {
+        insertRec(root->_left, user);
+    }
+    else {
+        insertRec(root->_right, user);
+    }
+}
+//===================================================================
+Node* insert() {
+    Input user;
+    Node* root = nullptr;
+
+    while (cin >> user.in_id >> user.in_x >> user.in_y) {  // Use EOF for input termination
+        insertRec(root, user);
+    }
+
+    return root;
+}
+//===================================================================
+void print_InOrder(Node* root) {
+    if (root) {
+        print_InOrder(root->_left);
+        cout << root->_id << ' ';
+        cout << root->_data->x << ' ';
+        cout << root->_data->y << ' ';
+        print_InOrder(root->_right);
+    }
+}
+//===================================================================
 int main()
 {
     Node* root = insert();
+    print_InOrder(root);
 
     return 0;
 }
